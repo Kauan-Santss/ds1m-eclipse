6df7453872a619da65606f3ebb1d0bc3f57bbdcd
KAUAN HENRIQUE SANTOS SILVA <20272562@cfp127aluno.local>
2021-01-12 11:26:06 -0300

    Segunda Versão 12-01-2021

--- Conta/src/br/senai/sp/jandira/model/Conta.java
+++ Conta/src/br/senai/sp/jandira/model/Conta.java
@@ -3,13 +3,21 @@ package br.senai.sp.jandira.model;
 public class Conta {
 	
 	public String tipo;
-	public double saldo;
+	private double saldo;
 	public String numero;
 	public String titular;
 	public boolean ativa;
 	
-	public void depositar() {
-		
+	public void depositar(double valorDeposito) {
+		if (valorDeposito < 0) {
+			System.out.println("O valor do deposito deve ser maior que zero");
+		}
+		else {
+			saldo = saldo + valorDeposito;
+		}
+	}
+	public void consultarSaldo() {
+		System.out.println("Saldo da Conta: R$" + saldo);
 	}
 	public void sacar() {
 		
