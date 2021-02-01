96cf7fdd4a0e2fadc323d12cfaf9091d26c3481a
KAUAN HENRIQUE SANTOS SILVA <20272562@cfp127aluno.local>
2021-02-01 11:21:20 -0300

    Aula 01-02-2021

--- retangulo/src/br/edu/empresa/gui/FrameRetangulo.java
+++ retangulo/src/br/edu/empresa/gui/FrameRetangulo.java
@@ -3,10 +3,14 @@ package br.edu.empresa.gui;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.function.DoublePredicate;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import br.edu.empresa.model.Retangulo;
@@ -38,12 +42,30 @@ public class FrameRetangulo {
 		JTextField textBase = new JTextField();
 		textBase.setBounds(50, 45, 100, 30);
 		
+		// *** Resultado da Area
+		JLabel labelArea = new JLabel();
+		labelArea.setText("Area:");
+		labelArea.setBounds(220, 10, 70, 30);
+		
+		// *** Resultado Perimetro
+		JLabel labelPerimetro = new JLabel();
+		labelPerimetro.setText("Perímetro:");
+		labelPerimetro.setBounds(220, 50, 180, 30);
+		
 		// *** Criar um botão para calcular
 		JButton buttonCalcular = new JButton();
 		buttonCalcular.setText("Calcular");
-		Color roxo = new Color(131,111,255);
-		buttonCalcular.setBackground(roxo);
+		Color verde = new Color(50,205,50);
+		buttonCalcular.setBackground(verde);
 		buttonCalcular.setBounds(10, 95, 180, 30);
+		
+		// *** Criar um botão para Limpar
+		JButton buttonLimpar= new JButton();
+		buttonLimpar.setText("Limpar");
+		Color vermelho = new Color(178,34,34);
+		buttonLimpar.setBackground(vermelho);
+		buttonLimpar.setBounds(10, 130, 180, 30);
+		
 		
 		
 		// ***Colocar dentro do painel de conteúdo (contentPane)
@@ -52,24 +74,28 @@ public class FrameRetangulo {
 		telaRetangulo.getContentPane().add(labelBase);
 		telaRetangulo.getContentPane().add(textBase);
 		telaRetangulo.getContentPane().add(buttonCalcular);
+		telaRetangulo.getContentPane().add(buttonLimpar);
+		telaRetangulo.getContentPane().add(labelArea);
+		telaRetangulo.getContentPane().add(labelPerimetro);
 		
 		telaRetangulo.setVisible(true);
 		
-		// *** Cliques da tela
+		// *** Ouvintes de ação
 		buttonCalcular.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				Retangulo r = new Retangulo();
-				r.setAltura(Double.parseDouble(textAltura.getText()));
-				r.setBase(Double.parseDouble(textBase.getText()));
+				Retangulo r1 = new Retangulo();
+				r1.setAltura(Double.parseDouble(textAltura.getText()));
+				r1.setBase(Double.parseDouble(textBase.getText()));
 				
-				labelAltura.setText(String.valueOf(r.calcularArea()));
-				labelBase.setText(String.valueOf(r.calcularPerimetro()));
+				labelArea.setText("Área: " + String.valueOf(r1.calcularArea()));
+				labelPerimetro.setText("Perímetro: " + String.valueOf(r1.calcularPerimetro()));
 				
 			}
 		});
+		
 	}
 	
 }
